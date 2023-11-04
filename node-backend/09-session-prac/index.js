48cc0bbd43556d2063ae603582ad254d1618094c
kihet77 <mssk2490@gmail.com>
2023-11-04 17:45:03 +0900

    study: cookies, session

---
+++ node-backend/09-session-prac/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const session = require("express-session");
+
+app.set("view engine", "ejs");
+//app.set("view engine", "ejs");
+app.use(session({
+    secret:"secret key",
+    resave:false,//모든 요청마다 session을 다시 저장할거냐?
+    saveUninitialized:true,//클라이언트가 처음 접속할때, 세션 초기화?
+    cookie:{
+        //httpOnly:true,
+        maxAge:3000
+    },
+    //secure:true, //https에서만 동장
+}));
+
+app.get("/", (req, res) => {
+    const user = req.session.user;
+    res.render("index",{user});
+    res.send({user:req.session.user});
+    console.log(req.session)
+});
+
+app.get("/set", (req, res) => {
+    req.session.user = "kit"
+    res.send({user:req.session.user});
+    console.log("2:",req.session)
+});
+
+// app.get("/get", (req, res)=>{
+//     console.log(req.session.user)
+//     res.send({user:req.session.user})
+// })
+
+app.listen(port, () => {
+  console.log("Server Port : ", port);
+});
