f0cf8ca7706e8737a34b1084f8ea7629e1258c6e
kihet77 <mssk2490@gmail.com>
2024-01-18 11:38:53 +0900

    java start

--- java/src/_01_basic_syntax/Prac.java
+++ java/src/_01_basic_syntax/Prac.java
@@ -1,15 +1,15 @@
 package _01_basic_syntax;
+
 import java.util.Scanner;
 
 public class Prac {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("이름을 입력하세요");
-        String userName = scanner.next();
-        System.out.println("나이를 입력하세요");
-        int userAge = scanner.nextInt();
-        System.out.println("안녕하세요"+ userName+"님 ("+ userAge + "세)");
+        System.out.println("이름을 입력하세요.");
+        String name = scanner.next();
+        System.out.println("나이를 입력하세요.");
+        int age = scanner.nextInt();
 
-        scanner.close();
+        System.out.println("안녕하세요."+name+"님 ("+age+"세)");
     }
 }
---
+++ java/src/_02_control_statement/Prac.java
@@ -0,0 +1,4 @@
+package _02_control_statement;
+
+public class Prac {
+}
